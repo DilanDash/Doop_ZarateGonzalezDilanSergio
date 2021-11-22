38613741e898784dc2b0dae5217c48d1b63b5d4f
DilanDash <ds.zarate.g@edu.utc.mx>
2021-11-21 23:29:36 -0600

    Array

---
+++ 10.-Tareaarreglos/Palidromo.java
@@ -0,0 +1,28 @@
+
+/*
+Elabora un programa que nos ayude a saber
+si un numero es palidromo
+*/
+
+import java.util.Scanner;
+
+public class palidromo{
+    public static void main(String[] args){
+        String palabra = "sometemos";
+
+        System.out.println("la palabra" + palabra + "es palidromo?:" + esPalidromo(palabra));
+
+
+    }
+    public static void main(String[] args){
+        palabra = palabra.toLowerCase();
+
+        for (int i = 0; j = palabra.length() -i; i = j; i++, j--){
+            if (palabra.chartAt(i) != palabra.chartAt(j)){
+                return false;
+
+            }
+        }
+        return true;
+    }
+}
